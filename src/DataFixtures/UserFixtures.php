a131f704ca6fe1a2d6364208879fb4073ac59992
soupra mani <soupramani@yahoo.fr>
2022-03-12 23:04:29 +0100

    Userfixtures modified

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,12 +5,12 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Symfony\Component\PasswordHasher\PasswordHasherInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
     private $passwordHasher;
-    public function __construct(PasswordHasherInterface $passwordHasher)
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
     {
         $this->passwordHasher = $passwordHasher;
     }
@@ -22,14 +22,14 @@ class UserFixtures extends Fixture
         $user->setDateInscription(new \DateTime());
         $user->setEmail('soupra@test.fr');
         $user->setRoles(['ROLE_ADMIN']);
-        $user->setPassword($this->passwordHasher->hash('test'));
+        $user->setPassword($this->passwordHasher->hashPassword($user, 'test'));
         $manager->persist($user);
         $user2 = new User();
         $user2->setNom('toto');
         $user2->setPrenom('titi');
         $user2->setDateInscription(new \DateTime("2020-12-24"));
         $user2->setEmail('toto@test.fr');
-        $user2->setPassword($this->passwordHasher->hash('test'));
+        $user2->setPassword($this->passwordHasher->hashPassword($user, 'test'));
         $manager->persist($user2);
         $manager->flush();
     }
